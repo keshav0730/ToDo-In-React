d09e57e6e567ca2079d11fd0aaa23d4946af0d56
keshav <kumarmishra0301@gmail.com>
2021-08-04 00:14:28 +0530

    initial Commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React  from 'react'
+
+const App=()=>
+{
+     return(
+         <> 
+         <div className="main_div">
+             <div className="center_div">
+                 <br />
+
+                 <h1>ToDo List</h1>
+                 <br />
+                 <input type="text" placeholder="Add a Items" />
+                 <button >+</button>
+             </div>
+
+         </div>
+
+         </>
+     )
+}
+ export default App;
