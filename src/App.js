e28829293544371b42e4148ebd50e9fa52afeb4e
keshav <kumarmishra0301@gmail.com>
2021-08-07 11:14:28 +0530

    Compltion of To DO list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState }  from 'react'
-
+import React, { useState }  from 'react';
+import ToDoList from './ToDoList.js';
 
 const App=()=>
 {
@@ -15,23 +15,41 @@ const App=()=>
           })
           setInputList("")
       }
+      const deleteItems=(id)=>
+      {
+          console.log("Item is deleted")
+          setItems((oldItems)=>{
+              return oldItems.filter((arrElem,index)=>{
+                    return index !==id;
+              })
+                 
+              
+          })
+      }
      return(
          <> 
          <div className="main_div">
              <div className="center_div">
                  <br />
 
+
                  <h1>ToDo List</h1>
+
                  <br />
+
                  <input type="text" placeholder="Add a Items" value={inputList} onChange={itemEvent} />
+
                  <button  onClick={listOfItems}>+</button>
+
+
                  <ol>
                      
-                    { items.map((itemval)=>{
-                         return <li>{itemval}</li>
+                    { items.map((itemval,index)=>{
+                        return <ToDoList key={index} id={index} text={itemval} onSelect={deleteItems} />
                      } )
                     }
                  </ol>
+
              </div>
 
          </div>
---
+++ src/ToDoList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+const TodoList = (props) => {
+   
+    return (
+        <>
+            <div className="todo_style">
+                
+            <i class="fas fa-times"  
+            onClick ={()=>{
+              props.onSelect(props.id)
+            }}
+            />
+
+
+                <li>{props.text}</li>
+
+            </div>
+
+        </>
+    );
+}
+export default TodoList;
